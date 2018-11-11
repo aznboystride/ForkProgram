1d85fcd877dac1fc5c4406837f5e8285dfa8a995
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-11-11 14:40:09 -0800

    added while loop that takes in user input each time

--- Main.cpp
+++ Main.cpp
@@ -5,5 +5,12 @@ using namespace std;
 
 int main()
 {
+  char user_choice[50];
+  cout << "[+] Enter String To Replace Followed By Replacement String: ";
+  cin >> user_choice;
+  while(strcmp(user_choice, "!wq")) {
+    cout << "[+] Enter String To Replace Followed By Replacement String: ";
+    cin >> user_choice;
+  }
   return 0;
 }
