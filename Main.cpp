9a1c07bd37fbfd1e83876d58b8cc2420820cf03d
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-11-11 14:47:29 -0800

    added <unistd.h> for fork() and added parent and child if statement

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 #include <sys/types.h>
 
 using namespace std;
@@ -8,9 +9,14 @@ int main()
   char user_choice[50];
   cout << "[+] Enter String To Replace Followed By Replacement String: ";
   cin >> user_choice;
+  pid_t pid = fork();
   while(strcmp(user_choice, "!wq")) {
-    cout << "[+] Enter String To Replace Followed By Replacement String: ";
-    cin >> user_choice;
+    if(!pid) {
+      // write child code
+    } else {
+      cout << "[+] Enter String To Replace Followed By Replacement String: ";
+      cin >> user_choice;
+    }
   }
   return 0;
 }
