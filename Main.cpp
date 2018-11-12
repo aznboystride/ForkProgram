ac2b84c7d7d64e5c14da61b3d220043c50822982
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-11-11 17:39:49 -0800

    program works now so far

--- Main.cpp
+++ Main.cpp
@@ -17,21 +17,25 @@ int main(int argc, char** argv)
 
   string user_choice;
 
-  pid_t pid = fork(); // Creates a child process
+  pid_t pid;
 
   while(true) {
-    if(!pid) { // Child process executes code here
-      // write child code
-    } else { // Parent process executes code here
-      cout << "[+]Enter String To Replace Followed By Replacement String: ";
-      getline(cin, user_choice);
-      if(user_choice == "!wq") {
-        break; // loop ends after user types in '!wq'
-      }
 
-      size_t spacepos = user_choice.find(" ");
+    cout << "[+]Enter String To Replace Followed By Replacement String: ";
+    getline(cin, user_choice);
+    if(user_choice == "!wq") {
+      break; // loop ends after user types in '!wq'
+    }
+    pid = fork(); // Creates a child process
+
+    if(pid) {
+      wait(NULL);
+    } else {
+      size_t spacepos = user_choice.find(0x20);
       int count = replace(user_choice.substr(0, spacepos), user_choice.substr(spacepos+1, user_choice.length()), argv[1]); // Count of replacements in file
       cout << count << endl;
+
+      return 0;
     }
   }
   return 0;
@@ -55,7 +59,7 @@ int replace(string str, string rep, string path) {
   }
   
   size_t pos;
-  while(pos = contents.find(str) != string::npos) {
+  while((pos = contents.find(str)) != string::npos) {
     contents.replace(pos, str.length(), rep);
     count++;
   }
