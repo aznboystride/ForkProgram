efd13b609ec25f1762c8d88cacf74e40f65de150
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-11-16 11:30:08 -0800

    commented c++ code and currently compiles

--- Main.c
+++ Main.c
@@ -1,49 +1,47 @@
-#include <iostream>
 #include <stdlib.h>
-#include <string>
-#include <fstream>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int replace(std::string, std::string, const char*);
+int replace(const char*, const char*, const char*);
 
 int main(int argc, char** argv) {
 
   if(argc != 2) {
-    std::cout << "Usage: " << argv[0] << " <path>\n";
+    printf("Usage: %s <path>", argv[0]);
     exit(1);
   }
-  std::fstream fs(argv[1], std::ios::in);
-  if(fs.fail()) {
-    std::cout << "Failure opening file: " << argv[1] << "\n";
-    exit(1);
-  }
-  fs.close();
+  // std::fstream fs(argv[1], std::ios::in);
+  // if(fs.fail()) {
+  //   std::cout << "Failure opening file: " << argv[1] << "\n";
+  //   exit(1);
+  // }
+  // fs.close();
 
-  int numOfReplacement;
-  std::string toReplace, replaceWith;
+  // int numOfReplacement;
+  // std::string toReplace, replaceWith;
 
-  while(true) {
+  // while(true) {
 
-    std::cout << "Enter string to replace or !wq to quit: "; 
-    getline(std::cin, toReplace);
+  //   std::cout << "Enter string to replace or !wq to quit: "; 
+  //   getline(std::cin, toReplace);
 
-    if(toReplace == "!wq")
-      break; // loop ends after user types in '!wq'
+  //   if(toReplace == "!wq")
+  //     break; // loop ends after user types in '!wq'
     
-    std::cout << "Enter string replacement: ";
-    getline(std::cin, replaceWith);
+  //   std::cout << "Enter string replacement: ";
+  //   getline(std::cin, replaceWith);
     
-    if(fork()) // creats child process
-      wait(NULL); // Wait for child process to terminate
-    else {
-      while(!(numOfReplacement = replace(toReplace, replaceWith, argv[1]))) // Inserted bug
-        std::cout << ".";
-      std::cout << "Replaced " << numOfReplacement << " occurences\n";
-      return 0;
-    }
-  }
+  //   if(fork()) // creats child process
+  //     wait(NULL); // Wait for child process to terminate
+  //   else {
+  //     while(!(numOfReplacement = replace(toReplace, replaceWith, argv[1]))) // Inserted bug
+  //       std::cout << ".";
+  //     std::cout << "Replaced " << numOfReplacement << " occurences\n";
+  //     return 0;
+  //   }
+  // }
   return 0;
 }
 
@@ -52,26 +50,8 @@ int main(int argc, char** argv) {
  * and replaces every occurence of string contained in the variable 'str' in that file  
  * with string 'rep'
  */
-int replace(std::string str, std::string rep, const char* path) {
+int replace(const char* str, const char* rep, const char* path) {
 	
-  std::fstream fs(path, std::ios::in);
-  std::string contents, line;
-  size_t pos;
-  int numOfReplacement = 0;
-
-  while(getline(fs, line))
-    contents += (line + "\n");
-  fs.close();
-
-  while((pos = contents.find(str)) != std::string::npos) {
-    contents.replace(pos, str.length(), rep);
-    numOfReplacement++;
-  }
-
-  fs.open(path, std::ios::out);
-  fs << contents;
-  fs.close();
-  
-  return numOfReplacement;
+  return 0;
 }
 
